4d179d06ecee4e276fda6dda81e82824f34b0dcf
Carolyn Cole <cam156@psu.edu>
2017-07-18 13:05:51 -0400

    Step wise refactor

--- app/services/field_configurator.rb
+++ app/services/field_configurator.rb
@@ -5,28 +5,32 @@ class FieldConfigurator
 
     # Defines what fields are shown on the search index view for each item
     def index_fields
-      get_fields(:index)
+      get_fields([:has_model, :date_uploaded])
     end
 
     # Defines what fields are shown on the work show page
     def show_fields
-      get_fields(:show)
+      get_fields([:date_uploaded, :depositor, :contributor,
+                         :date_modified, :date_created, :rights,
+                         :identifier, :description])
     end
 
     # Defines what fields are shown in the facets on the search index view
     def facet_fields
-      get_fields(:facet)
+      get_fields([:has_model, :collection, :file_format])
     end
 
     # Defines what fields are available to the user in the universal search
     def search_fields
-      get_fields(:search)
+      get_fields([:date_uploaded, :depositor, :contributor,
+                           :date_modified, :date_created, :rights,
+                           :identifier, :description, :title, :file_format])
     end
 
     private
 
-      def get_fields(type)
-        fields = {
+      def get_fields(type_specific_fields)
+        {
             resource_type: FieldConfig.new("Resource Type"),
             creator: FieldConfig.new(label: "Creator", facet_cleaners: [:titleize]),
             keyword:  FieldConfig.new(label: "Keyword", facet_cleaners: [:downcase]),
@@ -34,54 +38,31 @@ class FieldConfigurator
             language: FieldConfig.new("Language"),
             based_near: FieldConfig.new("Location"),
             publisher: FieldConfig.new(label: "Publisher", facet_cleaners: [:titleize]),
-        }
-        case type
-        when :index
-          fields.merge(has_model: FieldConfig.new(label: "Object Type",
-                                                              helper_method: :titleize,
-                                                              index_solr_type: :symbol,
-                                                              solr_type: :symbol),
-                       date_uploaded: FieldConfig.new(label: "Date Uploaded",
-                                                      index_solr_type: :stored_sortable,
-                                                      index_type: :date))
-        when :facet
-          fields.merge(has_model: FieldConfig.new(label: "Object Type",
-                                                              helper_method: :titleize,
-                                                              index_solr_type: :symbol,
-                                                              solr_type: :symbol),
-                       collection: FieldConfig.new(label: "Collection", helper_method: :collection_helper_method),
-            file_format: FieldConfig.new("File Format"))
-          when :show
-            fields.merge(date_uploaded: FieldConfig.new(label: "Date Uploaded",
-                                                                  index_solr_type: :stored_sortable,
-                                                                  index_type: :date),
-                         depositor:  FieldConfig.new("Depositor"),
-                         contributor: FieldConfig.new("Contributor"),
-                         date_modified: FieldConfig.new(label: "Date Modified",
-                                                                  index_solr_type: :stored_sortable,
-                                                                  index_type: :date),
-                         date_created: FieldConfig.new("Date Created"),
-                         rights: FieldConfig.new("Rights"),
-                         identifier: FieldConfig.new("Identifier"),
-                         description: FieldConfig.new("Description")
-            )
-        when :search
-          fields.merge(date_uploaded: FieldConfig.new(label: "Date Uploaded",
+        }.merge(all_type_specific_fields.select{|k,v| type_specific_fields.include?(k)})
+      end
+
+      def all_type_specific_fields
+        {
+            has_model:     FieldConfig.new(label: "Object Type",
+                                       helper_method: :titleize,
+                                       index_solr_type: :symbol,
+                                       solr_type: :symbol),
+            date_uploaded: FieldConfig.new(label: "Date Uploaded",
+                            index_solr_type: :stored_sortable,
+                            index_type: :date),
+            collection:    FieldConfig.new(label: "Collection", helper_method: :collection_helper_method),
+            file_format:   FieldConfig.new("File Format"),
+            depositor:  FieldConfig.new("Depositor"),
+            contributor: FieldConfig.new("Contributor"),
+            date_modified: FieldConfig.new(label: "Date Modified",
                                            index_solr_type: :stored_sortable,
                                            index_type: :date),
-                depositor:  FieldConfig.new("Depositor"),
-                contributor: FieldConfig.new("Contributor"),
-                date_modified: FieldConfig.new(label: "Date Modified",
-                                               index_solr_type: :stored_sortable,
-                                               index_type: :date),
-                date_created: FieldConfig.new("Date Created"),
-                rights: FieldConfig.new("Rights"),
-                identifier: FieldConfig.new("Identifier"),
-                description: FieldConfig.new("Description"),
-                title: FieldConfig.new("Title"),
-                file_format: FieldConfig.new("File Format")
-          )
-        end
+            date_created: FieldConfig.new("Date Created"),
+            rights: FieldConfig.new("Rights"),
+            identifier: FieldConfig.new("Identifier"),
+            description: FieldConfig.new("Description"),
+            title: FieldConfig.new("Title")
+        }
       end
   end
 end
